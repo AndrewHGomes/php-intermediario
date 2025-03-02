3b3a20e54c315c4a4a5683cfc3238b7a0cd4490d
AndrewHGomes <andrewvianigomes@gmail.com>
2025-03-02 10:51:53 -0300

    Início da playlist intermediária de PHP

---
+++ 00_introducao/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Introdução</title>
+</head>
+
+<body>
+  <header>
+    <h1>Introdução</h1>
+  </header>
+  <hr>
+  <main></main>
+</body>
+
+</html>
